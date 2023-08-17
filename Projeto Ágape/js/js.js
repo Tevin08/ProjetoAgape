8d952ec224e4b662ef62b38b7baeb05619a418a7
Tevin08 <114778525+Tevin08@users.noreply.github.com>
2023-08-17 17:51:42 -0300

    Add files via upload

---
+++ Projeto Ágape/js/js.js
@@ -0,0 +1,37 @@
+//teste
+
+function uploadimg(){ 
+    const formData = new
+    FormData(document.querySelector("#upload-form") );
+
+    fetch()
+}
+
+
+
+
+
+
+// function mudarcor1() {
+//     const  botao = document.querySelector('#btn-doadorA')
+//     const botaoC = document.querySelector("#btn-doadorC:hover")
+//     if( botaoC!== null){
+
+//         botao.style.backgroundColor = '#f088c7' ;
+//         botao.style.color='#fff'
+
+//         botaoC.style.backgroundColor = '#fff' ;
+//         botaoC.style.color='#f088c7'
+        
+//     }
+
+     
+//  }
+
+//  mudarcor2=()=>{
+//     const  botao = document.querySelector('#btn-doadorA')
+
+//         botao.style.backgroundColor = '#f088c7' ;
+//         botao.style.color='#fff'
+    
+//  }
