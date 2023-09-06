43cb59ebcf93dd9ff1913a8edadd53859a817a41
tevin08 <estevaopsg08@gmail.com>
2023-09-06 15:24:12 -0300

    update

--- Projeto Ágape 1.8/cadastroD.php
+++ Projeto Ágape 1.8/cadastroD.php
@@ -2,7 +2,7 @@
     include "banco.php";
     function gravar($conexao){
 
-        $sql = "insert into tb_doador(nm_doador, nm_usuario, email, senha, documento) values('{$POST['nome-completo']}, '{$POST['nomeusuario']}, '{$POST['email']}, '{$POST['senha']}, '{$POST['documento']})";
+        $sql = "insert into tb_doador(nm_doador, nm_user, email, senha, cpf) values('{$POST['nome-completo']}, '{$POST['nomeusuario']}, '{$POST['email']}, '{$POST['senha']}, '{$POST['documento']})";
         return mysqli_query($conexao, $sql);
 
     }
