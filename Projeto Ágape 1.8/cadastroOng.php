51da8ec3ddd7f64aeef7d75974a10b0b507166a5
Tevin08 <estevaopsg08@gmail.com>
2023-09-08 17:50:58 -0300

    simply update

--- Projeto Ágape 1.8/cadastroOng.php
+++ Projeto Ágape 1.8/cadastroOng.php
@@ -1,7 +1,7 @@
 <?php
     include "banco.php";
     function gravar($conexao) {
-        $sql = "insert to tb_ong(nm_representante, nm_ong, email, senha, documento) values('{$POST['nome-representante']}, '{$POST['nome-ong']}, {$POST['email']}, {$POST['senha']}, {$POST['documento']}";
+        $sql = "insert to tb_ong(nm_representante, nm_ong, email, senha, cnpj) values('{$POST['nome-representante']}, '{$POST['nome-ong']}, {$POST['email']}, {$POST['senha']}, {$POST['documento']}";
         return mysqli_query($conexao, $sql);
     }
 ?>
