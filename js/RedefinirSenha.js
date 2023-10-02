be2e510494d44b347aaf3997406afce1e981eba4
tevin08 <estevaopsg08@gmail>
2023-10-02 00:59:57 -0300

    Page redefinir Senha

--- js/RedefinirSenha.js
+++ js/RedefinirSenha.js
@@ -1,12 +1,24 @@
-function modalShow (){
-    const modal = document.querySelector(".container-modal");
-    
-    modal.style.display = "flex"
-    
-  }
+function modalShow() {
+  const modal = document.querySelector(".container-modal");
 
-  function modalClose() {
-    const modal = document.querySelector(".container-modal");
-    
-    modal.style.display = "none"
-  }
+  modal.style.display = "flex";
+}
+
+function modalClose() {
+  const modal = document.querySelector(".container-modal");
+
+  modal.style.display = "none";
+}
+
+function animationbtn() {
+  const btn = document.querySelector("#btn-modal-confirm");
+  btn.style.width = "50px";
+  btn.style.height = "50px";
+  btn.style.borderRadius = "50%";
+  btn.textContent = "\u2713";
+  setInterval(() => {
+
+    btn.type = "submit";
+  }, 30);
+  
+}
