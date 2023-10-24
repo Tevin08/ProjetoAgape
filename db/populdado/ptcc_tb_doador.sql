22400348f1cc9a2a6ce973ecaaf2c3edcb9143fc
Thauanni <141364610+Thauanni@users.noreply.github.com>
2023-10-24 13:59:21 -0300

    Add files via upload

---
+++ db/populdado/ptcc_tb_doador.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: ptcc
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tb_doador`
+--
+
+DROP TABLE IF EXISTS `tb_doador`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_doador` (
+  `cd_doador` int NOT NULL AUTO_INCREMENT,
+  `nm_doador` varchar(80) DEFAULT NULL,
+  `nm_user` varchar(80) DEFAULT NULL,
+  `cpf` char(11) DEFAULT NULL,
+  `cnpj` char(14) DEFAULT NULL,
+  `senha` varchar(60) DEFAULT NULL,
+  `email` varchar(90) DEFAULT NULL,
+  PRIMARY KEY (`cd_doador`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_doador`
+--
+
+LOCK TABLES `tb_doador` WRITE;
+/*!40000 ALTER TABLE `tb_doador` DISABLE KEYS */;
+INSERT INTO `tb_doador` VALUES (1,'Thauanni','Thata','56688160870',NULL,'12345','thauannic.10@gmail.com'),(2,'Mary Ellen','Mary','30450678830',NULL,'12345','moraismaryellen@gmail.com'),(3,'Estevão ','Tevin','55419332850',NULL,'12354','estevaoferreira@gmail.com');
+/*!40000 ALTER TABLE `tb_doador` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-10-17 15:16:56
